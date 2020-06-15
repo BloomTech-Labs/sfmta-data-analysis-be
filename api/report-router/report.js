22ce8421748100b8035ca355bb4f12a47d8ca500
nick migel <nickmigel2@gmail.com>
2020-06-15 11:45:46 -0400

    only All route has a route_id now?

--- api/report-router/report.js
+++ api/report-router/report.js
@@ -19,7 +19,7 @@ router.post('/type', (req, res) => {
     db.getAll()
         .then(data => {
             let report = Array.from(data.report).filter(item => item.route_id === req.body.route_type)
-            res.status(200).json(report[0])
+            res.status(200).json(data)
         })
         .catch(err => {
             res.status(500).json(err.message)
